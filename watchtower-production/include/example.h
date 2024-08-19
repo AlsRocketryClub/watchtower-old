d9ad8e79ed763ec1e020e23e1e7f91d32e2af407
Hans Verdolaga <hans.codedeitech@gmail.com>
2024-08-19 19:25:13 +0200

    Repository cleanup

--- watchtower-production/include/example.h
+++
@@ -1,4 +0,0 @@
-// test_functions.h
-#pragma once
-
-double add(double a, double b);
--- watchtower-prototype/pybind_tests/example.py
+++
@@ -1,7 +0,0 @@
-# example.py in watchtower-prototype/pybind_tests/
-import example
-
-print(example.add(2, 3))
-
-# Keyboard press to exit
-input("Press Enter to continue...")
