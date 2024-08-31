631ced10e9376d5c74394db4c35bd4034261afd9
Hans Verdolaga <hans.codedeitech@gmail.com>
2024-08-31 10:48:58 +0200

    Main fresh start

--- watchtower-production-tests/pch.h
+++
@@ -1,8 +0,0 @@
-//
-// pch.h
-//
-
-#pragma once
-
-#include "gtest/gtest.h"
-#include "../watchtower-production/include/test_functions.h"
--- watchtower-production/src/test_functions.cpp
+++
@@ -1,6 +0,0 @@
-// test_functions.cpp
-#include "test_functions.h"
-
-double add(double a, double b) {
-	return a + b;
-}
